4ca9017747307a3b63739a42ac563b83557b9bbd
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2022-09-23 00:04:22 +0530

    feat:fix:api

--- pages/api/GetFormInfo.js
+++ pages/api/GetFormInfo.js
@@ -2,16 +2,15 @@
 
 import { supabase } from "../../config";
 
-export default function PostInfo(req, res) {
+export default async function PostInfo(req, res) {
   const { reg_no, name, email, phone, password } = req.body;
 
-  supabase.from("aryans").insert({
+  const { data, err } = await supabase.from("aryans").insert({
     name: name,
     email: email,
     phone: phone,
     password: password,
     regno: reg_no,
   });
-
-  res.status(200).json({ added: "👍" });
+  res.status(200).json({ added: data });
 }
